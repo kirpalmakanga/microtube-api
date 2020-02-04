8edeb8e232d582dd8ae21cbff588d676aac66287
Kirpal <makanga.kirpal@gmail.com>
2020-02-04 02:49:33 +0100

    setup socket events and broadcasting

--- index.js
+++ index.js
@@ -1,18 +1,71 @@
 const express = require('express');
 const socketIO = require('socket.io');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8081;
 
-const server = express().listen(3000, () =>
+const server = express().listen(8081, () =>
     console.log(`Listening on ${PORT}`)
 );
 
 const io = socketIO(server);
 
+const connectedDevices = {};
+
+const syncDevices = (userId) => {
+    if (connectedDevices[userId]) {
+        io.to(userId).emit('devices:sync', [
+            ...connectedDevices[userId].values()
+        ]);
+    }
+};
+
+const addDevice = (device, userId) => {
+    if (!connectedDevices[userId] || !connectedDevices[userId].size) {
+        connectedDevices[userId] = new Map();
+
+        device.isMaster = true;
+    }
+
+    connectedDevices[userId].set(device.deviceId, device);
+
+    syncDevices(userId);
+};
+
+const removeDevice = (deviceId, userId) => {
+    if (connectedDevices[userId]) {
+        connectedDevices[userId].delete(deviceId);
+    }
+
+    syncDevices(userId);
+};
+
+const setMasterDevice = (deviceId, userId) => {
+    for (const [id, data] of connectedDevices[userId]) {
+        connectedDevices[userId].set(id, {
+            ...data,
+            isMaster: id === deviceId
+        });
+    }
+
+    syncDevices(userId);
+};
+
 io.on('connection', (socket) => {
-    console.log('Client connected');
+    socket.on('room', (userId) => {
+        socket.join(userId);
 
-    socket.on('disconnect', () => console.log('Client disconnected'));
+        socket.on('device:add', (device) => {
+            addDevice(device, userId);
+
+            socket.on('disconnect', () =>
+                removeDevice(device.deviceId, userId)
+            );
+        });
+
+        socket.on('device:active', (deviceId) =>
+            setMasterDevice(deviceId, userId)
+        );
+
+        socket.on('devices:sync', () => syncDevices(userId));
+    });
 });
-
-//
