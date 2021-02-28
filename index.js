2f3b843e9e208fee6dbc2d002cf61253ca9f2482
Kirpal <makanga.kirpal@gmail.com>
2021-02-28 22:09:09 +0100

    updated origins

--- index.js
+++ index.js
@@ -9,11 +9,8 @@ const server = express().listen(PORT, () =>
 
 const io = socketIO(server, {
     cors: {
-        origin: [
-            'http://localhost:8080',
-            'https://microtube.netlify.app:*',
-            'https://microtube.netlify.com:*'
-        ]
+        origin:
+            'http://localhost:8080 https://microtube.netlify.app:* https://microtube.netlify.com:*'
     }
 });
 
